0a962f9bbbadfbb23a1bf57461faf928bc4ca499
Gulya1098 <mansurova221098@gmail.com>
2024-12-11 15:27:34 +0500

    Add files via upload

---
+++ 'module_1_5.py'.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 3, 'Name'
+print(immutable_var)
+#immutable_var[1] = 10
+#print(immutable_var)
+mutable_list = ([10, 20, 30], False)
+mutable_list[0][1] = 200
+print(mutable_list)
